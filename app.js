2b372062da5547a79b5b6b4d07e3de7e936e4805
Alejae1998 <alejagr1998@gmail.com>
2022-11-07 17:11:32 -0800

    adding more css, importing my functions, adding code to app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 /* Imports */
-
+import { renderBerries, renderGrass, renderCow } from './utils.js';
 /* Get DOM Elements */
+const cowsEl = document.querySelector('.cow');
+const cowInputEl = document.getElementById('cow-name');
+const berriesEl = document.querySelector('.berries');
+const grassEl = document.querySelector('.grass');
+const addBerrieButton = document.getElementById('add-berrie-button');
 
 /* State */
 
