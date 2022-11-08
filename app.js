017c458edc86df98c08564731ed65f8d5458318b
Alejae1998 <alejagr1998@gmail.com>
2022-11-07 17:20:23 -0800

    adding my cowData to the array

--- app.js
+++ app.js
@@ -6,9 +6,35 @@ const cowInputEl = document.getElementById('cow-name');
 const berriesEl = document.querySelector('.berries');
 const grassEl = document.querySelector('.grass');
 const addBerrieButton = document.getElementById('add-berrie-button');
+const addGrassButton = document.getElementById('add-grass-button');
+const addCow = document.getElementById('add-cow-button');
 
 /* State */
+let berriesCount = 3;
+let grassCount = 3;
 
+const cowData = [
+    {
+        name: 'Bessie',
+        starving: '2',
+    },
+    {
+        name: 'Clarabella',
+        starving: '1',
+    },
+    {
+        name: 'Magic',
+        starving: '1',
+    },
+    {
+        name: 'Snowflake',
+        starving: '2',
+    },
+    {
+        name: 'Sprinkles',
+        starving: '2',
+    },
+];
 /* Events */
 
 /* Display Functions */
